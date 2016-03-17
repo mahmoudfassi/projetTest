e908bba650274dfacb378365252d10368b7f30e4
Mahmoud Fassi Fihri <Mahmoud Fassi Fihri>
2016-03-17 23:50:51 +0000

    test

--- src/main/java/fr/ebiznext/App.java
+++ src/main/java/fr/ebiznext/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World testx!" );
+        System.out.println( "Hello World testxx!" );
     }
 }
