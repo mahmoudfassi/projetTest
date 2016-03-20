a4b214d26c5de8656525eee7194b0f44434a094a
mahmoudfassi <mahmoudfassi@hotmail.fr>
2016-03-20 14:34:41 +0000

    new feature added

--- src/main/java/fr/ebiznext/App.java
+++ src/main/java/fr/ebiznext/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" )
+        System.out.println( "Hello World!" );
+        Person p=new Person();
+        p.setNom("Fassi");
     }
 }
